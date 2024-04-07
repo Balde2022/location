3b4eebe51c52bd5e17f879bd9daefd286b6a4bc8
Balde <abdourahamane620@gmail.com>
2024-04-07 06:57:40 +0200

    Implementaion du service

---
+++ src/main/java/com/ocprojecttree/location/Services/Jwt/AuthenticationService.java
@@ -0,0 +1,53 @@
+package com.ocprojecttree.location.Services.Jwt;
+
+import com.ocprojecttree.location.Models.Users.AuthenticationResponse;
+import com.ocprojecttree.location.Models.Users.User;
+import com.ocprojecttree.location.Repository.Users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtServiceImp jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthenticationService(UserRepository repository, PasswordEncoder passwordEncoder, JwtServiceImp jwtService, AuthenticationManager authenticationManager) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public AuthenticationResponse register(User request){
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setCreated_at(request.getCreated_at());
+        user.setUpdated_at(request.getUpdated_at());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        user = repository.save(user);
+        String token = jwtService.generateToken(user);
+        return new AuthenticationResponse(token);
+    }
+
+    public AuthenticationResponse authenticate(User request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail() ,
+                        request.getPassword()
+                )
+        );
+        User user = repository.findByEmail(request.getEmail()).orElseThrow();
+        String token = jwtService.generateToken(user);
+
+        return  new AuthenticationResponse(token);
+    }
+}
