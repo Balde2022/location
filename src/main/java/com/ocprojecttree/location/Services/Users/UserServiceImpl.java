41269c3ae33dc37f2e1d77acd8771b1a63bb29a9
Balde <abdourahamane620@gmail.com>
2024-04-07 06:57:25 +0200

    Implementaion du service

---
+++ src/main/java/com/ocprojecttree/location/Services/Users/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ocprojecttree.location.Services.Users;
+
+import com.ocprojecttree.location.Repository.Users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found")
+                );
+    }
+}
